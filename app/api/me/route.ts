1c8d8c5ec013ffd94ef8729d531f9d9dc49f5582
Tim Lorent <tim.lorent@icloud.com>
2026-02-25 18:39:38 +0100

    refactor: remove redundant route file

--- app/api/me/route.ts
+++
@@ -1,7 +0,0 @@
-import { getAdmin } from '@/lib/admin';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const isAdmin = await getAdmin();
-  return NextResponse.json({ isAdmin });
-}
